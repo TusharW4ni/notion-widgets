dd374217283843c208ddbbc3a7563b35dcb05aa7
TusharW4ni <reachtusharwani@gmail.com>
2024-09-09 19:42:45 -0500

    styled the div on event-countdown

--- pages/event-countdown/index.vue
+++ pages/event-countdown/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="flex h-screen">
     <div
-      class="flex w-screen h-screen rounded-lg border-2 border-gray-400 bg-emerald-500"
+      class="flex w-screen h-screen rounded-lg border-4 border-[#3c6650] bg-[#243d30]"
     >
       <div class="flex justify-center items-center w-full">
-        <div class="underline uppercase text-4xl font-mono">Hello</div>
+        <div class="underline uppercase text-4xl font-mono text-white">
+          Hello
+        </div>
       </div>
     </div>
   </div>
