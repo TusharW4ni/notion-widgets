0c3b3ff7ee64f0472356e2966ae6853841dde211
TusharW4ni <reachtusharwani@gmail.com>
2024-09-09 19:37:33 -0500

    added event counter placeholder

---
+++ pages/event-countdown/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex h-screen">
+    <div
+      class="flex w-screen h-screen rounded-lg border-2 border-gray-400 bg-emerald-500"
+    >
+      <div class="flex justify-center items-center w-full">
+        <div class="underline uppercase text-4xl font-mono">Hello</div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
+<script setup>
+async function navigateToEventCounter() {
+  await navigateTo("/event-countdown");
+}
+</script>
+
 <template>
   <div class="flex h-screen justify-center items-center">
-    <div class="underline uppercase font-bold font-mono text-4xl">Hello</div>
+    <button
+      v-on:click="navigateToEventCounter()"
+      class="border p-2 rounded bg-gray-100 hover:bg-gray-300 hover:shadow-lg"
+    >
+      Event Counter
+    </button>
   </div>
 </template>
