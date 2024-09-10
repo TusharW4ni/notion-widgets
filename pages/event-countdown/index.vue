6a6b91ed38991ffc91588057ba4652d1ffd79259
TusharW4ni <reachtusharwani@gmail.com>
2024-09-09 19:59:22 -0500

    made countdown to semester end

--- pages/event-countdown/index.vue
+++ pages/event-countdown/index.vue
@@ -1,11 +1,44 @@
+<script setup>
+const timer = ref("Loading...");
+
+onMounted(() => {
+  const countDownDate = new Date("December 05, 2024 23:59:59").getTime();
+
+  const countdownFunction = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const months = Math.floor(distance / (1000 * 60 * 60 * 24 * 30));
+    const days = Math.floor(
+      (distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24)
+    );
+    const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (distance < 0) {
+      clearInterval(countdownFunction);
+      timer.value = "EXPIRED";
+    } else {
+      timer.value = `${months}M ${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }
+  }, 1000);
+});
+</script>
+
 <template>
   <div class="flex h-screen">
     <div
       class="flex w-screen h-screen rounded-lg border-4 border-[#3c6650] bg-[#243d30]"
     >
-      <div class="flex justify-center items-center w-full">
-        <div class="underline uppercase text-4xl font-mono text-white">
-          Hello
+      <div class="flex flex-col justify-center items-center w-full">
+        <div class="uppercase text-xl font-mono font-bold text-gray-400">
+          Semester Ends
+        </div>
+        <div class="text-2xl font-mono font-bold text-white mt-4">
+          {{ timer }}
         </div>
       </div>
     </div>
